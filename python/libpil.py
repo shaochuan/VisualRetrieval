7feff1c25645461e6c0eda7c5310297b52ff1131
Shao-Chuan Wang <shaochuan.wang@gmail.com>
2011-03-05 00:22:47 -0500

    [what] * Add dendrogram, scatterplot, texture

--- python/libpil.py
+++ python/libpil.py
@@ -7,6 +7,7 @@
 
 import pdb
 import matplotlib.pyplot as plt
+import matplotlib.mpl as mpl
 
 import array
 try:
@@ -22,16 +23,26 @@ try:
 except ImportError:
     print "You haven't installed numpy, have you?"
 
+
+class ConversionException(Exception):
+    pass
+
 def PIL2array(img):
     return numpy.array(img.getdata(),
-                    numpy.uint8).reshape(img.size[0], img.size[1], 3)
+                    numpy.uint8).reshape(img.size[1], img.size[0], 3)
 
 def array2PIL(arr, size):
-    mode = 'RGBA'
-    arr = arr.reshape(arr.shape[0]*arr.shape[1], arr.shape[2])
-    if len(arr[0]) == 3:
-        arr = numpy.c_[arr, 255*numpy.ones((len(arr),1), numpy.uint8)]
-    return Image.frombuffer(mode, size, arr.tostring(), 'raw', mode, 0, 1)
+    if len(arr.shape) == 3:
+        mode = 'RGBA'
+        arr = arr.reshape(arr.shape[0]*arr.shape[1], arr.shape[2])
+        if len(arr[0]) == 3:
+            arr = numpy.c_[arr, 255*numpy.ones((len(arr),1), numpy.uint8)]
+        return Image.frombuffer(mode, size, arr.tostring(), 'raw', mode, 0, 1)
+    elif len(arr.shape) == 2:
+        mode = 'L'
+        return Image.frombuffer(mode, size, arr.tostring(), 'raw', mode, 0, 1)
+    else:
+        raise ConversionException("I don't know how to convert this array.")
 
 def concat_imgs(img1, img2, *args, **argd):
     ori = argd.get('ori', 'v')
@@ -69,11 +80,11 @@ def hist(grayimg, bins=20, rang=(0,255), plot=False):
         fig = plt.figure()
         ax = fig.add_subplot(111)
         #bincenters = 0.5*(edges[1:]+edges[:-1])
-        #ax.bar(edges[:-1], hi, edges[1]-edges[0], facecolor='green', alpha=0.75)
+        ax.bar(edges[:-1], hi, edges[1]-edges[0], facecolor='green', alpha=0.75)
         #h2, edges2, patches = ax.hist(grayimg.flatten(), 
         #                bins, rang, normed=True, facecolor='green', alpha=0.75)
         #assert all(edges2 == edges)
-        #plt.show()
+        plt.show()
     
     return hi, edges
 
@@ -82,13 +93,39 @@ def concathist(rgbarr):
     hr, edges = hist(r)
     hg = hist(g)[0]
     hb = hist(b)[0]
-    #plothists(hr, hg, hb, edges)
     return hr, hg, hb
+
+def rgbarr2gray(rgbarr):
+    r,g,b = split3array(rgbarr)
+    return 0.3*r+0.59*g+0.11*b
+
+import colorsys
+
+def rgbarr2hsvarr(rgbarr):
+    r,g,b = split3array(rgbarr)
+    if r.dtype.type == numpy.uint8:
+        r = numpy.float32(r)
+        g = numpy.float32(g)
+        b = numpy.float32(b)
+        r /= 255.0
+        g /= 255.0
+        b /= 255.0
+    vfunc = numpy.vectorize(colorsys.rgb_to_hsv)
+    return vfunc(r,g,b)
+
+def lapacian(grayarr):
+    try:
+        from scipy.stsci.convolve import convolve2d
+    except ImportError:
+        print "You probabably didn't install python scipy 0.7.0 correctly."
+    kernel = -1*numpy.ones((3,3))
+    kernel[1,1] = 8.0
+    return convolve2d(grayarr, kernel, fft=True)
 
 import profile
 
 @profile.timing
-def rgbhist(rgbarr, rbins=4, gbins=8, bbins=4, blackbin=[(0,0,0)]):
+def rgbhist(rgbarr, rbins=4, gbins=8, bbins=4, blackbin=[]):
     cdef int i, j, w, h
     cdef int r, g, b
     cdef int rbase, gbase, bbase
@@ -110,6 +147,9 @@ def rgbhist(rgbarr, rbins=4, gbins=8, bbins=4, blackbin=[(0,0,0)]):
     hist = hist/sum(hist.flatten())
     return hist.flatten()
 
+def laphist(grayarr, bins=400):
+    lap = lapacian(grayarr)
+    return hist(lap, bins, rang=(-256*8, 256*8))[0]
 
 @profile.timing
 def onenorm_inverse(h1, h2):
@@ -128,21 +168,47 @@ def plothists(h1, h2, h3, edges):
     ax.bar(edges[:-1], h3, edges[1]-edges[0], facecolor='blue', alpha=0.35)
     plt.show()
 
+
+from matplotlib.offsetbox import AnnotationBbox, OffsetImage
+
+def scatterplot(xs, ys, imgs):
+    n_imgs = len(imgs)
+    fig = plt.figure(figsize=(8,5),dpi=200)
+    ax = fig.add_subplot(111)
+    ax.set_xlim(xs.min()-0.1*xs.max(), 1.1*xs.max())
+    ax.set_ylim(ys.min()-0.1*ys.max(), 1.1*ys.max())
+    for i in xrange(n_imgs):
+        imagebox = OffsetImage(imgs[i], zoom=0.5)
+        ab = AnnotationBbox(imagebox, (xs[i], ys[i]),
+                        xybox=(0,0),
+                        xycoords='data',
+                        boxcoords="offset points",
+                        pad=0)
+        ax.add_artist(ab)
+
+    cmap = mpl.cm.hsv
+    norm = mpl.colors.Normalize(vmin=0, vmax=1)
+    plt.draw()
+    axbar = fig.add_axes([0.18, 0.05, 0.68, 0.05])
+    cb1 = mpl.colorbar.ColorbarBase(axbar, cmap=cmap,
+                                   norm=norm,
+                                   orientation='horizontal')
+    fig.savefig('scatterplot.png',dpi=200)
+
+
 def loadImage(filename):
     return Image.open(filename)
 
 
 def main():
-    img1 = loadImage('i01.jpg')
-    img2 = loadImage('../../Images/i06.jpg')
+    #img1 = loadImage('i01.jpg')
+    img1 = loadImage('../../Images/i06.jpg')
     
     arr = PIL2array(img1)
-    rgbhist(arr)
-    return
-    hi1 = concathist(arr)
-    arr = PIL2array(img2)
-    hi2 = concathist(arr)
-    print hist_intersection(hi1, hi2)
+    hi, edges = laphist(arr)
+    plt.imshow(lapacian(rgbarr2gray(arr)))
+
+    
 #img2 = array2PIL(arr, img.size)
     #img2.save('test.jpg')
 
--- python/main.py
+++ python/main.py
@@ -8,7 +8,7 @@ import pdb
 DEFAULT_IMAGE_ROOT_FOLDER = r'../../Images'
 
 def get_image_folder():
-    if len(sys.argv) > 2:
+    if '-i' in sys.argv:
         idx = sys.argv.index('-i')
         return sys.argv[idx+1]
     return DEFAULT_IMAGE_ROOT_FOLDER
@@ -16,7 +16,11 @@ def get_image_folder():
 import profile
 
 @profile.timing
-def calkernel(imgs, metric=libpil.hist_intersection):
+def colorkernel(imgs, 
+        metric=libpil.hist_intersection,
+        cachedfn='color_kernel.pkl'):
+    if os.path.exists(cachedfn):
+        return numpy.load(cachedfn)
     n_imgs = len(imgs)
 
     # calculation of kernel
@@ -31,13 +35,40 @@ def calkernel(imgs, metric=libpil.hist_intersection):
     print kernel
     return kernel
 
+@profile.timing
+def texturekernel(imgs, 
+        metric=libpil.onenorm_inverse,
+        cachedfn='texture_kernel.pkl'):
+    if os.path.exists(cachedfn):
+        return numpy.load(cachedfn)
+    n_imgs = len(imgs)
+    # calculation of kernel
+    kernel = numpy.zeros((n_imgs, n_imgs))
+    for i in xrange(n_imgs):
+        arr1 = libpil.PIL2array(imgs[i])
+        gray1 = libpil.rgbarr2gray(arr1)
+        for j in xrange(i):
+            arr2 = libpil.PIL2array(imgs[j])
+            gray2 = libpil.rgbarr2gray(arr2)
+            kernel[i,j] = metric(libpil.laphist(gray1),
+                                 libpil.laphist(gray2))
+    kernel = kernel + numpy.transpose(kernel)
+    print kernel
+    return kernel
+
+
 def output_compare_imgs(imgs, kernel, outfilename='out.png'):
+    n_imgs = len(imgs)
     # most similar indices
     most_similar = kernel.argmax(1)
+    p,q = (kernel.argmax() % n_imgs, kernel.argmax() / n_imgs)
     for i in xrange(0, len(imgs)):
         kernel[i,i] = 1
     # least similar indices
     least_similar = kernel.argmin(1)
+    s,t = (kernel.argmin() % n_imgs, kernel.argmin() / n_imgs)
+    for i in xrange(0, len(imgs)):
+        kernel[i,i] = 0
     
     catimgs = []
     for i, ML in enumerate(zip(most_similar, least_similar)):
@@ -50,15 +81,93 @@ def output_compare_imgs(imgs, kernel, outfilename='out.png'):
     canvas = libpil.concat_imgs(*catimgs)
     canvas.save(outfilename)
 
+    mostalike_imgpair = libpil.concat_imgs(imgs[p], imgs[q], ori='h')
+    leastalike_imgpair = libpil.concat_imgs(imgs[s], imgs[t], ori='h')
+    mostalike_imgpair.save(os.path.splitext(outfilename)[0] + '_mostalike.png')
+    leastalike_imgpair.save(os.path.splitext(outfilename)[0] + '_leastalike.png')
 
+def pdist(kernel):
+    import generator
+    m,n = kernel.shape
+    assert m==n
+    dst = numpy.zeros((m*(m-1)/2,1))
+    k = 0
+    for i, j in generator.combinations(xrange(m),2):
+        dst[k] = 1-kernel[i,j]
+        k += 1
+    return dst.flatten()
+
+def output_dendrogram(imgs, kernel, method='complete', dend_fn='_dendrogram.png'):
+    dst = pdist(kernel)
+    links = linkage(dst, method=method)
+    tmp_dend_fn = method + '_' + dend_fn
+    axis = dendrogram(links, 
+               orientation='left',
+               figsize=(7,12),
+               outfilename=tmp_dend_fn)[1]
+    figimg = libpil.loadImage(tmp_dend_fn)
+    labels = [label._text for label in axis.get_yticklabels()]
+    labels = map(int, labels)
+    labels.reverse()
+    for i, ind in enumerate(labels):
+        imgs[ind].thumbnail((30,30))
+        offset = i*(imgs[ind].size[1]+4) + 120
+        figimg.paste(imgs[ind], (52,offset))
+    figimg.save('fig_'+tmp_dend_fn)
+
+
+from hcluster import linkage, dendrogram
 def main():
     image_folder = get_image_folder()
     imgfiles = glob.glob(os.path.join(image_folder, "*.jpg"))
     imgs = map(libpil.loadImage, imgfiles)
-    kernel = calkernel(imgs)
-    output_compare_imgs(imgs, kernel, outfilename='hist_insection.png')
-    kernel = calkernel(imgs, metric=libpil.onenorm_inverse)
-    output_compare_imgs(imgs, kernel, outfilename='onenorm.png')
+    if 'color' in sys.argv:
+        color_kernel = colorkernel(imgs, metric=libpil.onenorm_inverse)
+        output_compare_imgs(imgs, color_kernel, outfilename='color.png')
+        color_kernel.dump('color_kernel.pkl')
+    if 'texture' in sys.argv:
+        texture_kernel = texturekernel(imgs)
+        output_compare_imgs(imgs, texture_kernel, outfilename='texture.png')
+        texture_kernel.dump('texture_kernel.pkl')
+    
+
+    if 'color' in sys.argv and 'texture' in sys.argv:
+        # 1-norm kernel normalization
+        color_kernel /= max(color_kernel.sum(1))
+        texture_kernel /= max(texture_kernel.sum(1))
+        for s in xrange(0, 11, 2):
+            r = s * 0.1
+            kernel = texture_kernel * r + (1-r) * color_kernel
+            kernel /= kernel.max()
+            output_compare_imgs(imgs,
+                    kernel, outfilename='r%.1f.png' % (r,))
+            for method in ('single', 'complete'):
+                output_dendrogram(imgs, 
+                                  kernel, 
+                                  method=method,
+                                  dend_fn='r%.1f_dendrogram.png' % (r,))
+
+    if 'scatter' in sys.argv:
+        n_imgs = len(imgs)
+        dominant_colors = numpy.zeros((n_imgs,))
+        for i in xrange(n_imgs):
+            hue = libpil.rgbarr2hsvarr( libpil.PIL2array(imgs[i]) )[0]
+            histhue,edges = libpil.hist(hue, bins=40, rang=(0,1))
+            bincenters = 0.5*(edges[1:]+edges[:-1])
+            print 'dominant hue:', bincenters[ histhue.argmax() ]
+            dominant_colors[i] = bincenters[ histhue.argmax() ]
+        total_textures = numpy.zeros((n_imgs,))
+        for i in xrange(n_imgs):
+            lapimg = libpil.lapacian(
+                     libpil.rgbarr2gray(
+                     libpil.PIL2array(
+                         imgs[i] )))
+            lapimg = lapimg.flatten()
+            meadtext = numpy.median(numpy.abs(lapimg))
+            print 'meandian texture:', meadtext
+            total_textures[i] = meadtext
+        
+        libpil.scatterplot(dominant_colors, total_textures, imgs)
 
 
 if __name__ == '__main__':
